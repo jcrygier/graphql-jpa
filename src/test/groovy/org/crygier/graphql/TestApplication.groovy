c74b2721f85c91507d0ac332cc68cd4e24d6c2ce
John Crygier <john@crygier.com>
2016-01-14 08:12:27 -0600

    Upgrade dependencies.
    - Add Depenency on Spring Web
    - Remove explicit H2 server, as it's included in Spring Boot 1.3

--- src/test/groovy/org/crygier/graphql/TestApplication.groovy
+++ src/test/groovy/org/crygier/graphql/TestApplication.groovy
@@ -1,10 +1,8 @@
 package org.crygier.graphql
 
-import groovy.transform.CompileDynamic
 import groovy.transform.CompileStatic
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass
 import org.springframework.boot.orm.jpa.EntityScan
 import org.springframework.context.ApplicationContext
 import org.springframework.context.annotation.Bean
@@ -23,21 +21,6 @@ class TestApplication {
     @Bean
     public GraphQLExecutor graphQLExecutor() {
         return new GraphQLExecutor();
-    }
-
-    @Bean
-    @CompileDynamic
-    @ConditionalOnClass(name = ["org.h2.tools.Server"])
-    public Object h2WebServer() {
-        String[] realArguments = [ "-webPort", "8082", "-webAllowOthers" ] as String[]
-        String[][] arguments = [ realArguments ] as String[][]
-
-        try {
-            Object server = Class.forName("org.h2.tools.Server").getDeclaredMethod("createWebServer", String[]).invoke(null, arguments);
-            server.start()
-
-            return server;
-        } catch (Exception ignored) {}
     }
 
 }
